74ac947539b4242e2fdabedd75f088e678e8a2d4
ahmetburkan <ahmetburkan@outlook.com>
2019-12-13 16:36:11 +0100

    up

--- src/App/controller/AppController.java
+++ src/App/controller/AppController.java
@@ -2,22 +2,50 @@ package App.controller;
 
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 public class AppController {
 
+    @FXML
+    private Button uitvoeren;
 
     @FXML
-    private ChoiceBox selectbox;
+    private ChoiceBox<String> dierkeuze;
 
     @FXML
-    private ChoiceBox selectbox1;
+    private ChoiceBox bezoekerkeuze;
+
+    @FXML
+    private TextField handeling;
 
     @FXML
     private Text result;
 
-    @FXML
-    private TextField wat;
+//uitvoeren.setOnAction(e -> getDierKeuze(dierkeuze));
+
+    private void getDierKeuze(ChoiceBox<String> dierkeuze) {
+        String keuze = dierkeuze.getValue();
+        System.out.println(keuze);
+    }
+
+    private void getHandelingInput(TextField handeling) {
+        String input = handeling.getText();
+        System.out.println(input);
+    }
+
+    private void getBezoekerKeuze(ChoiceBox<String> bezoekerkeuze) {
+        String keuze = bezoekerkeuze.getValue();
+        System.out.println(keuze);
+
+    }
+
+    private void onSubmit()  {
+//        getDierKeuze();
+//        getHandelingInput();
+//        getBezoekerKeuze();
+
+    }
 }
